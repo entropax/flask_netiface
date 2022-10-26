70832b9973cc6a7282e8d665c364e071bb922a88
Sol 'Entropax' Maxwell <sol@entropax.ru>
2022-10-26 14:45:01 +0300

    feature: create fucntion for working with 'ip a' linux command with sudo list of function - switch_interface_status - add_ip_address - delete_ip_address - change_ip_address - chante_ip_mask

--- flask_netiface/net_interface.py
+++ flask_netiface/net_interface.py
@@ -33,3 +33,68 @@ def get_interfaces_info():
                 interfaces[interface]['MAC'] = address
 
     return interfaces
+
+
+def switch_interface_status(interface, status, password):
+    password = bytes(password, encoding='utf-8')
+    if status == 'UP':
+        cmd = f'sudo -S ip link set dev {interface} down'.split()
+        subprocess.run(
+            cmd,
+            shell=False,
+            input=password,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+    else:
+        cmd = f'sudo -S ip link set dev {interface} up'.split()
+        subprocess.run(
+            cmd,
+            shell=False,
+            input=password,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+
+
+def add_ip_address(interface, address, password):
+    password = bytes(password, encoding='utf-8')
+    cmd = f'sudo -S ip a add {address} dev {interface}'.split()
+    subprocess.run(
+            cmd,
+            shell=False,
+            input=password,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+
+
+def delete_ip_address(interface, address, password):
+    password = bytes(password, encoding='utf-8')
+    cmd = f'sudo -S ip a del {address} dev {interface}'.split()
+    subprocess.run(
+            cmd,
+            shell=False,
+            input=password,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+
+
+def change_ip_address(interface, new_ip, old_ip, password):
+    add_ip_address(interface, new_ip, password)
+    delete_ip_address(interface, old_ip, password)
+
+
+def change_ip_mask(interface, ip_address, new_mask, password):
+    password = bytes(password, encoding='utf-8')
+    delete = f'sudo -S ip a del {ip_address} dev {interface}'.split()
+    add = f'sudo -S ip a add {ip_address}/{new_mask} dev {interface}'.split()
+    subprocess.run(
+            delete,
+            shell=False,
+            input=password,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+    subprocess.run(
+            add,
+            shell=False,
+            input=password,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
